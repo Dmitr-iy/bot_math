b12888daac1999c5afa1e113f80aa044a3ccabda
dmitriy <kosheld@inbox.ru>
2024-06-26 22:49:16 +0500

    refactor

--- data/get_bd.py
+++ data/get_bd.py
@@ -1,6 +1,5 @@
 import psycopg2
 import logging
-
 from data.config import config_settings
 
 
@@ -8,7 +7,6 @@ def execute_query(query, params=None, fetch_all=True):
     conn = None
     cursor = None
     result = None
-
     try:
         conn = psycopg2.connect(
             dbname=config_settings.db_name,
@@ -17,21 +15,16 @@ def execute_query(query, params=None, fetch_all=True):
             host=config_settings.db_host
         )
         cursor = conn.cursor()
-
         cursor.execute(query, params)
-
         if fetch_all:
             result = cursor.fetchall()
         else:
             result = cursor.fetchone()
-
     except Exception as e:
         logging.error(f"Error executing SQL query: {e}", exc_info=True)
-
     finally:
         if cursor:
             cursor.close()
         if conn:
             conn.close()
-
     return result
