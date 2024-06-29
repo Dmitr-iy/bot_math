b84d8741ecc0967f2b7a4cdb23f089bbdc06a71b
dmitriy <kosheld@inbox.ru>
2024-06-29 17:16:53 +0500

    add bd MySQL

--- bots/utils/commands.py
+++ bots/utils/commands.py
@@ -5,10 +5,22 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(
-            command="start", description="Начать"
+            command="start", description="Старт"
         ),
         BotCommand(
             command="help", description="Помощь"
+        ),
+        BotCommand(
+            command="info", description="Информация"
+        ),
+        BotCommand(
+            command='message', description='Сообщение'
+        ),
+        BotCommand(
+            command='task', description='Задание'
+        ),
+        BotCommand(
+            command='all tem', description='Все темы'
         ),
     ]
     await bot.set_my_commands(commands, scope=BotCommandScopeDefault())
